fe496c18aca120440a3e4e5d8c6f7b0dedafde20
SomberTM <sombertm@gmail.com>
2022-10-08 18:03:39 -0400

    Daily work

--- server/src/controllers/samples.ts
+++ server/src/controllers/samples.ts
@@ -1,4 +1,5 @@
 import prisma from "../db";
+import { DateTime } from "luxon";
 
 async function getSamples(req: any, res: any) {
     const samples = await prisma.samples.findMany();
@@ -12,11 +13,13 @@ function getSample(req: any, res: any) {
 async function createSample(req: any, res: any) {
     const sample = req.body
     try {
+        console.log(`Creating sample ${JSON.stringify(sample, null, 4)}`)
         const newSample = await prisma.samples.create({
             data: sample
         })
         res.status(201).json(newSample)
     } catch (error: any) {
+        console.log(error)
         res.status(500).json({ message: error.message })
     }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import bodyParses from 'body-parser'
+import bodyParser from 'body-parser'
 import cors from 'cors'
 
 import { config } from 'dotenv'; config()
@@ -11,11 +11,11 @@ import sampleRoutes from './routes/samples'
     const app: express.Express = express()
     const port = process.env.PORT ?? 5000
 
-    app.use('/samples', sampleRoutes)
-
-    app.use(bodyParses.json({ limit: '50mb' }))
-    app.use(bodyParses.urlencoded({ limit: '50mb', extended: true }))
+    app.use(bodyParser.json({ limit: '50mb' }))
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
     app.use(cors());
+
+    app.use('/samples', sampleRoutes)
 
     const server = app.listen(port, () => {
         console.log(`Server is running on port ${port}`)
