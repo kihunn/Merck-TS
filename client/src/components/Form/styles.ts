44506b85a414ad488e32e2e6f5bb9d4e01a09eb3
Thomas Hughes <50004094+SomberTM@users.noreply.github.com>
2022-10-12 14:41:02 -0400

    Update styles.ts

--- client/src/components/Form/styles.ts
+++ client/src/components/Form/styles.ts
@@ -1,1 +1,22 @@
+import { makeStyles } from "@mui/styles";
 
+export default makeStyles(() => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: 1,
+    },
+  },
+  paper: {
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    // @ts-ignore
+    padding: 10,
+  },
+  form: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+  },
+  buttonSubmit: {
+    marginBottom: 10,
+  },
+}));
