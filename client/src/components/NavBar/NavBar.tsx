c4481c429d42a83a6233003414f256ebd6391f9a
SomberTM <sombertm@gmail.com>
2022-12-01 15:53:04 -0500

    Updated Styles

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -17,35 +17,41 @@ const NavBar = (props: any) => {
                 <Typography className={classes.heading} variant="h2" align="center">Merck Label Dashboard</Typography>
                 <img className={classes.image} src={msdlogo} alt="MSD Logo" height="60" />
                 
-                <Button
-                    variant='contained'
-                >
-                    <Link to="/" style={linkStyle}>Home</Link>
-                </Button>
+                <div>
+                    <Button
+                        variant='contained'
+                    >
+                        <Link to="/" style={linkStyle}>Home</Link>
+                    </Button>
+                </div>
 
-                <Button
-                    variant='contained'
-                >
-                    <Link to="/psamples/create" style={linkStyle}>Create Sample - Pharmaceutical Team</Link>
-                </Button>       
-                    
-                <Button
-                    variant='contained'
-                >
-                    <Link to="/psamples" style={linkStyle}>View Samples - Pharmaceutical Team</Link>
-                </Button>
+                <div>
+                    <Button
+                        variant='contained'
+                    >
+                        <Link to="/psamples" style={linkStyle}>View Samples - Pharmaceutical Team</Link>
+                    </Button>
+                    <span></span>
+                    <Button
+                        variant='contained'
+                    >
+                        <Link to="/psamples/create" style={linkStyle}>Create Sample - Pharmaceutical Team</Link>
+                    </Button> 
+                </div>
 
-                <Button
-                    variant='contained'
-                >
-                    <Link to="/samples" style={linkStyle}>View Samples - AR&D Team</Link>
-                </Button>
-
-                <Button
-                    variant='contained'
-                >
-                    <Link to="/samples/create" style={linkStyle}>Create Sample - AR&D Team</Link>
-                </Button>    
+                <div>
+                    <Button
+                        variant='contained'
+                    >
+                        <Link to="/samples" style={linkStyle}>View Samples - AR&D Team</Link>
+                    </Button>
+                    <span></span>
+                    <Button
+                        variant='contained'
+                    >
+                        <Link to="/samples/create" style={linkStyle}>Create Sample - AR&D Team</Link>
+                    </Button>   
+                </div> 
 
 
             </AppBar>
--- client/src/components/NavBar/styles.ts
+++ client/src/components/NavBar/styles.ts
@@ -1,20 +1,22 @@
 import { makeStyles } from '@mui/styles'
+import theme from '../../styles';
 
 export default makeStyles((theme) => ({
     appBar: {
-        backgroundColor: 'rgba(255, 255, 255, 1)',
+        backgroundColor: 'rgba(0, 0, 0, 255)',
+        color: 'rgba(247, 247, 247, 255)',
         borderRadius: 15,
         margin: '30px 0',
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     heading: {
-        color: 'rgba(0, 137, 119, 255)'
+        color: 'rgba(0, 133, 124, 255)'
     },
     image: {
-        alignItems: 'right',
-        marginLeft: '15px'
+        textAlign: 'center',
+        display: 'inline',
     }
 }));
