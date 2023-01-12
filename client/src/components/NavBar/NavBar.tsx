51dbfadc052244d754f3b6ae07da37b4f07045b7
SomberTM <sombertm@gmail.com>
2023-01-12 12:59:23 -0500

    Working on styles

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,10 +1,11 @@
-import { AppBar, Typography, Button, Container, Grow } from '@mui/material';
+import { AppBar, Typography, Button, Container, Grow, Box, Toolbar } from '@mui/material';
 import useStyles from './styles'
 import msdlogo from '../../images/msdlogo.png'
 import { Link } from 'react-router-dom';
+import "./styles.css";
 
 const NavBar = (props: any) => {
-    const classes = useStyles()
+    // const classes = useStyles()
 
     const linkStyle = {
         textDecoration: 'none',
@@ -13,48 +14,51 @@ const NavBar = (props: any) => {
 
     return (        
         <Container>
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Merck Label Dashboard</Typography>
-                <img className={classes.image} src={msdlogo} alt="MSD Logo" height="60" />
-                
-                <div>
-                    <Button
-                        variant='contained'
-                    >
-                        <Link to="/" style={linkStyle}>Home</Link>
-                    </Button>
-                </div>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" color="inherit" style={{ padding: '10px', margin: '10px' }}>
+                    <Toolbar>
+                        <Typography variant="h4" align="center" color="primary" component="div">
+                            Merck Label Dashboard
+                        </Typography>
 
-                <div>
-                    <Button
-                        variant='contained'
-                    >
-                        <Link to="/psamples" style={linkStyle}>View Samples - Pharmaceutical Team</Link>
-                    </Button>
-                    <span></span>
-                    <Button
-                        variant='contained'
-                    >
-                        <Link to="/psamples/create" style={linkStyle}>Create Sample - Pharmaceutical Team</Link>
-                    </Button> 
-                </div>
+                        {/* <Button
+                            variant='contained'
+                        >
+                            <Link to="/" style={linkStyle}>Home</Link>
+                        </Button> */}
 
-                <div>
-                    <Button
-                        variant='contained'
-                    >
-                        <Link to="/samples" style={linkStyle}>View Samples - AR&D Team</Link>
-                    </Button>
-                    <span></span>
-                    <Button
-                        variant='contained'
-                    >
-                        <Link to="/samples/create" style={linkStyle}>Create Sample - AR&D Team</Link>
-                    </Button>   
-                </div> 
+                        <div className="app-toolbar-button-container">
+                            <Button
+                                variant='contained'
+                            >
+                                <Link to="/psamples" style={linkStyle}>View Samples - Pharmaceutical Team</Link>
+                            </Button>
 
+                            <Button
+                                variant='contained'
+                            >
+                                <Link to="/psamples/create" style={linkStyle}>Create Sample - Pharmaceutical Team</Link>
+                            </Button>
 
-            </AppBar>
+                            <Button
+                                variant='contained'
+                            >
+                                <Link to="/samples" style={linkStyle}>View Samples - AR&D Team</Link>
+                            </Button>
+
+                            <Button
+                                variant='contained'
+                            >
+                                <Link to="/samples/create" style={linkStyle}>Create Sample - AR&D Team</Link>
+                            </Button>  
+                        </div> 
+
+                        <Link to="/" style={linkStyle}>
+                            <img src={msdlogo} alt="MSD Logo" height="60"/>
+                        </Link>
+                    </Toolbar>
+                </AppBar>
+            </Box>
             <Grow>
                 <Container>
                     {props.children}
